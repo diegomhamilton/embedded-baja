fe05e35f613bd26dfb0445e610a05baa5a2e9478
Diego Hamilton <diegomhamilton@github.com>
2018-10-12 23:59:13 -0300

    Implement accelerometer/gyroscope routines, change sampling frequencies

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include "structures.h"
 #include "CANMsg.h"
 #include "CANDef.h"
-
+#include "LSM6DS3.h"
+#include "Kalman.h"
 
 /* Communication protocols */
 CAN can(PB_8, PB_9);
 Serial uart(PA_9, PA_10);
-//I2C i2c(PB_7, PB_6);
+// I2C i2c(PB_7, PB_6);
 
 /* Debug section */
 uint64_t    time_CAN = 0,
@@ -28,8 +29,8 @@ InterruptIn freq_sensor(PB_4);                                  // frequency sen
 // AnalogIn    temperature();
 
 /* Timers */
-Ticker tick_25hz;                                               // timer for 25Hz acquisitions
-Ticker tick_100hz;                                              // timer for 100Hz acquisitions
+Ticker tick_10hz;                                               // timer for 10hz acquisitions
+Ticker tick_25hz;                                              // timer for 25hz acquisitions
 Ticker tick_serial;                                             // timer for serial send 
 //Timer pulse_timer;
 
@@ -39,17 +40,18 @@ uint16_t pulse_counter = 0;
 uint16_t current_period = 0;
 uint32_t last_count = Kernel::get_ms_count();                   // store moment of last interrupt (PB_4)
 CircularBuffer <state_t, STATE_BUF_SIZE> state_buffer;          // store sequence of events to proccess
-CircularBuffer <packet_100hz_t, ACQ_BUF_SIZE> acq100hz_buffer;  // store packets of accelerometer and gyro
+CircularBuffer <packet_25hz_t, ACQ_BUF_SIZE> acq25hz_buffer;  // store packets of accelerometer and gyro
 
 /* Function declarations */
 void freq_ISR();
 void choke_ISR();
 void run_ISR();
+void tick_10hz_ISR();
 void tick_25hz_ISR();
-void tick_100hz_ISR();
 void tick_serial_ISR();
 void CANRX_check();
 uint16_t filter_msg(CANMsg& msg);
+void gyro_getdata(uint16_t accx, uint16_t accy, uint16_t accz, uint16_t grx, uint16_t gry, uint16_t grz, uint16_t dt);
 
 void main()
 {
@@ -58,6 +60,7 @@ void main()
     uint16_t frequency;
     uint64_t last_acq = 0;
     CANMsg txmsg;
+    LSM6DS3 LSM6DS3(PB_7, PB_6);
     /* RTOS set up */
     osThreadId id = osThreadGetId();                        //get id from main thread
     osThreadSetPriority(osThreadGetId(), osPriorityNormal); // set priority of main thread 1 level before CANRX_thread
@@ -66,8 +69,12 @@ void main()
     can.frequency(5000000);                                 // set CAN rate to 1Mbps
     signal_wave.period(0.02f);                              // set frequency to 50hz
     signal_wave.write(0.5f);                                // set 50% duty cycle
-    tick_25hz.attach(tick_25hz_ISR, 0.1);                   // configure FREQ state ticker for 10Hz
-    tick_100hz.attach(tick_100hz_ISR, 0.01);                 // configure FREQ state ticker for 10Hz
+    LSM6DS3.begin(LSM6DS3.G_SCALE_245DPS,   \
+                   LSM6DS3.A_SCALE_2G,      \
+                   LSM6DS3.G_ODR_26_BW_2,       \
+                   LSM6DS3.A_ODR_26);                       // set 245DPS gyro scale, set 2G acc scale, set sampling frequency to 104Hz
+    tick_10hz.attach(tick_10hz_ISR, 0.1);                   // configure FREQ state ticker for 10Hz
+    tick_25hz.attach(tick_25hz_ISR, 0.04);                 // configure FREQ state ticker for 10Hz
     tick_serial.attach(tick_serial_ISR, 0.5);               // configure SERIAL state ticker for 2Hz
     freq_sensor.fall(freq_ISR);                             // |
     choke.fall(choke_ISR);                                  // |->attach interrupt callback
@@ -79,34 +86,39 @@ void main()
     {
         if (state_buffer.full())
             uart.printf("buffer full");
-        else if(!state_buffer.empty())
+        if(!state_buffer.empty())
             state_buffer.pop(st);
         else
-            st = ACQ;
+            st = IDLE;
         
         switch(st)
         {
             case ACQ:
-                uart.printf("*\r\n");                       // debug
-                if(!acq100hz_buffer.empty())
+                LSM6DS3.readAccel();                        // read accelerometer data into LSM6DS3.aN_raw
+                LSM6DS3.readGyro();                         //  "   gyroscope data into LSM6DS3.gN_raw
+                data->sample_25hz.dt = Kernel::get_ms_count() - last_acq;
+                last_acq = Kernel::get_ms_count();
+                data->sample_25hz.accx = LSM6DS3.ax_raw;
+                data->sample_25hz.accy = LSM6DS3.ay_raw;
+                data->sample_25hz.accz = LSM6DS3.az_raw;
+                data->sample_25hz.dpsx = LSM6DS3.gx_raw;
+                data->sample_25hz.dpsy = LSM6DS3.gy_raw;
+                data->sample_25hz.dpsz = LSM6DS3.gz_raw;
+                gyro_getdata(data->sample_25hz.accx, data->sample_25hz.accy, data->sample_25hz.accz, \
+                             data->sample_25hz.dpsx, data->sample_25hz.dpsy, data->sample_25hz.dpsz, \
+                             data->sample_25hz.dt);
+                /* Send accelerometer data */
+                txmsg.clear();
+                txmsg.id = ACC_Msg;
+                txmsg << data->sample_25hz.accx << data->sample_25hz.accy << data->sample_25hz.accz;
+                if(can.write(txmsg))
                 {
-                    acq100hz_buffer.pop(data->sample_100hz);
-                    // calculate roll and pitch
-                    /* Send accelerometer data */
+                    /* Send gyroscope data only if accelerometer data succeeds */
                     txmsg.clear();
-                    txmsg.id = ACC_Msg;
-                    txmsg << data->sample_100hz.accx << data->sample_100hz.accy << data->sample_100hz.accz;
-                    if(can.write(txmsg))
-                    {
-                        /* Send gyroscope data only if accelerometer data succeeds */
-                        txmsg.clear();
-                        txmsg.id = DPS_Msg;
-                        txmsg << data->sample_100hz.dpsx << data->sample_100hz.dpsy << data->sample_100hz.dpsz << data->sample_100hz.timestamp;
-                        can.write(txmsg);
-                    }
+                    txmsg.id = DPS_Msg;
+                    txmsg << data->sample_25hz.dpsx << data->sample_25hz.dpsy << data->sample_25hz.dpsz << data->sample_25hz.dt;
+                    can.write(txmsg);
                 }
-                else if(acq100hz_buffer.full())
-                    uart.printf("ACQ F\r\n");               // debug
                 break;
             case CHKRUN:
                 data->chk_run = !choke.read() << 1 | !run.read() << 0;
@@ -117,7 +129,7 @@ void main()
                 break;
             case FREQ:
                 freq_sensor.fall(NULL);                     // disable interrupt
-                data->sample_25hz.speed = 1000*((float)pulse_counter/current_period);    //calculates frequency in Hz
+                data->sample_10hz.speed = 1000*((float)pulse_counter/current_period);    //calculates frequency in Hz
                 // makes conversion to speed
                 pulse_counter = 0;                          //|
                 current_period = 0;                         //|-> reset pulses related variables
@@ -125,18 +137,21 @@ void main()
                 freq_sensor.fall(freq_ISR);                 // enable interrupt
                 /* Send frequency */
                 txmsg.clear();
-                txmsg << data->sample_25hz.speed << (uint16_t) 0;
+                txmsg.id = SPD_Msg;
+                txmsg << data->sample_10hz.speed << (uint16_t) 0;
                 can.write(txmsg);
                 break;
             case SERIAL:
                 // formats string to send to display or radio
-                uart.printf("speed: %d\r\n", data->sample_25hz.speed);
-                uart.printf("dpsx = %d\r\n", data->sample_100hz.dpsx);
+                uart.printf("speed: %d\r\n", data->sample_10hz.speed);
+                uart.printf("dpsx = %d\r\n", data->sample_25hz.dpsx);
                 uart.printf("CR %d\r\n", data->chk_run);    // debug
                 uart.printf("calls = %d\r\n", calls_CAN);
                 calls_CAN = 0;
                 break;
             default:
+                uart.printf("*\r\n");                       // debug
+                Thread::wait(2);
                 break;
         }
     }
@@ -159,12 +174,12 @@ void run_ISR()
     state_buffer.push(CHKRUN);
 }
 
-void tick_25hz_ISR()
+void tick_10hz_ISR()
 {
     state_buffer.push(FREQ);
 }
 
-void tick_100hz_ISR()
+void tick_25hz_ISR()
 {
     state_buffer.push(ACQ);
 }
@@ -198,7 +213,6 @@ void CANRX_check()
             wait_time = 10;
             timeout = 0;
         }
-        // todo implement timeout for wait_time == 0
         CANRX_thread.wait(wait_time);
     }
 }
@@ -209,21 +223,21 @@ uint16_t filter_msg(CANMsg& msg)
     uint16_t wait_time = 10;    // default wait time in ms
     if(msg.id == ACC_Msg)
     {
-        msg >> data->sample_100hz.accx >> data->sample_100hz.accy >> data->sample_100hz.accz;
+        msg >> data->sample_25hz.accx >> data->sample_25hz.accy >> data->sample_25hz.accz;
         wait_time = 0;
     }
     else if(msg.id == DPS_Msg)
     {
-        msg >> data->sample_100hz.dpsx >> data->sample_100hz.dpsy >> data->sample_100hz.dpsz >> data->sample_100hz.timestamp;
-        acq100hz_buffer.push(data->sample_100hz);
+        msg >> data->sample_25hz.dpsx >> data->sample_25hz.dpsy >> data->sample_25hz.dpsz >> data->sample_25hz.dt;
+       acq25hz_buffer.push(data->sample_25hz);
     }
     else if (msg.id == RPM_Msg)
     {
-        msg >> data->sample_25hz.rpm >> data->sample_25hz.last_rpm;
+        msg >> data->sample_10hz.rpm >> data->sample_10hz.last_rpm;
     }
     else if(msg.id == SPD_Msg)
     {
-        msg >> data->sample_25hz.speed >> data->sample_25hz.last_spd;
+        msg >> data->sample_10hz.speed >> data->sample_10hz.last_spd;
     }
     else if(msg.id == FLAGS_Msg)
     {
@@ -231,6 +245,51 @@ uint16_t filter_msg(CANMsg& msg)
     }
 
     return wait_time;
+}
+
+/* Method adapted from Kristian Lauszus library example, source: https://github.com/TKJElectronics/KalmanFilter*/
+void gyro_getdata(uint16_t accx, uint16_t accy, uint16_t accz, uint16_t grx, uint16_t gry, uint16_t grz, uint16_t dt){
+    static Kalman kalmanX, kalmanY;
+    float kalAngleX, kalAngleY;
+    float pitch, roll;
+    float gyroXrate, gyroYrate;
+    float ax, ay, az;
+    static bool first_execution = true;
+    
+    ax = (float) accx * TO_G;
+    ay = (float) accy * TO_G;
+    az = (float) accz * TO_G; 
+    pitch = atan2(ay, az) * RAD_TO_DEGREE;
+    roll = atan(-ax / sqrt(ay * ay + az * az)) * RAD_TO_DEGREE;
+    gyroXrate = grx / TO_DPS;                            // Convert to deg/s
+    gyroYrate = gry / TO_DPS;                            // Convert to deg/s
+
+    if (first_execution)
+    {
+        // set starting angle if first execution
+        first_execution = false;
+        kalmanX.setAngle(roll);
+        kalmanY.setAngle(pitch);
+    }
+    // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+    if((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90))
+    {
+        kalmanX.setAngle(roll);
+        kalAngleX = roll;
+    }
+    else
+    {
+        kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+    }
+
+    if(abs(kalAngleX) > 90)
+    {
+        gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
+    }
+    kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
+
+    data->roll = kalAngleX;
+    data->pitch = kalAngleY;
 }
 
 /* Packet methods definition */
@@ -241,18 +300,20 @@ Packet::Packet()
 
 void Packet::clear()
 {
-    sample_100hz.accx = 0;
-    sample_100hz.accy = 0;
-    sample_100hz.accz = 0;
-    sample_100hz.dpsx = 0;
-    sample_100hz.dpsy = 0;
-    sample_100hz.dpsz = 0;
-    sample_100hz.timestamp = 0;
-    sample_25hz.rpm = 0;
-    sample_25hz.speed = 0;
-    sample_25hz.last_spd = 0;
-    sample_25hz.last_rpm = 0;
+    sample_25hz.accx = 0;
+    sample_25hz.accy = 0;
+    sample_25hz.accz = 0;
+    sample_25hz.dpsx = 0;
+    sample_25hz.dpsy = 0;
+    sample_25hz.dpsz = 0;
+    sample_25hz.dt = 0;
+    sample_10hz.rpm = 0;
+    sample_10hz.speed = 0;
+    sample_10hz.last_spd = 0;
+    sample_10hz.last_rpm = 0;
     sample_1hz.temp = 0;
     sample_1hz.count = 0;
+    roll = 0;
+    pitch = 0;
     chk_run = 0x00;
 }
