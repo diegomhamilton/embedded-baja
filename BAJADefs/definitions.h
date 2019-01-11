99480baebf414d4aff5a8e8635560751858b5c0d
Diego Hamilton <diegomhamilton@github.com>
2019-01-11 09:14:29 -0300

    Add data strucutures Remove registers redefinition (already defined at low-level libraries)

--- BAJADefs/definitions.h
+++ BAJADefs/definitions.h
@@ -1,6 +1,30 @@
-#define CAN_RF0R        (*((volatile unsigned long *)0x4000640C))
-#define CAN_FLAG_FMP0   ((uint32_t)0x12000003) /*!< FIFO 0 Message Pending Flag */
-#define CAN_RF0R_RFOM0 ((uint8_t)0x20) /*!<Release FIFO 0 Output Mailbox */
-#define CAN_RF1R        (*((volatile unsigned long *)0x40006410))
-#define CAN_IER         (*((volatile unsigned long *)0x40006414))
-#define CAN_IER_FMPIE0  ((uint32_t)0x00000002) /*!<FIFO Message Pending Interrupt Enable */
+typedef struct
+{
+    uint16_t acc_x;
+    uint16_t acc_y;
+    uint16_t acc_z;
+    uint16_t gyro_x;
+    uint16_t gyro_y;
+    uint16_t gyro_z;
+} imu_t;
+    
+typedef struct
+{
+    uint16_t rpm;
+    uint16_t vel;
+    uint8_t flags;      // MSB - BOX | SERVO_ERROR | NC | NC | NC | NC | CHK | RUN - LSB
+} acq_10hz_t;
+    
+typedef struct
+{  
+    uint8_t motor;
+    uint8_t cvt;
+} temperature_t;
+    
+typedef struct
+{  
+    imu_t imu[4];
+    acq_10hz_t data_10hz[2];
+    temperature_t temp;
+    uint8_t data_saved;
+} packet_t;
