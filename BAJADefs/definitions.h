3eb3a70a998329f3dd0f4d10c2ebb7d9931dd534
Diego Hamilton <diegomhamilton@github.com>
2019-01-15 07:52:24 -0300

    Add CAN messages ID's

--- BAJADefs/definitions.h
+++ BAJADefs/definitions.h
@@ -12,7 +12,17 @@
 #define THROTTLE_MID    0x00
 #define THROTTLE_RUN    0x01
 #define THROTTLE_CHOKE  0x02
-#define THROTTLE_ID     0x100
+
+#define SYNC_ID         0x001       // message for bus sync
+#define THROTTLE_ID     0x100       // 1by = throttle state (0x00, 0x01 or 0x02)
+#define FLAGS_ID        0x101       // 1by
+#define IMU_ACC_ID      0x200       // 8by = accelerometer data (3D) + timestamp
+#define IMU_DPS_ID      0x201       // 8by = gyroscope data (3D) + timestamp 
+#define SPEED_ID        0x300       // 4by = speed + timestamp
+#define RPM_ID          0x304       // 4by = rpm + timestamp
+#define TEMPERATURE_ID  0x400       // 4by = engine temp. + cvt temp. + timestamp
+#define FUEL_ID         0x500       // 3by = fuel level + timestamp
+
 
 typedef struct
 {
