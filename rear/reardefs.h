b0f1ec3d2af988df60f0789088cace3cf6beac37
Diego Hamilton <diegomhamilton@github.com>
2019-01-14 23:34:51 -0300

    Add include guard and change state names

--- rear/reardefs.h
+++ rear/reardefs.h
@@ -1,13 +1,15 @@
+#define REARDEFS_H
+
 #define SERVO_RUN         866
 #define SERVO_MID         1320
 #define SERVO_CHOKE       1780
 
 typedef enum
 {
-    IDLE,           // wait
-    SLOW_ACQ,       // acquire temperatures and fuel data
-    RPM_ACQ,        // calculate speed
-    THROTTLE,       // write throttle position (PWM)
-    RADIO_TRANSMIT, // send data for box via radio (SPI)
-    DEBUG           // send data for debug
+    IDLE_ST,        // wait
+    SLOWACQ_ST,     // acquire temperatures and fuel data
+    RPM_ST,         // calculate speed
+    THROTTLE_ST,    // write throttle position (PWM)
+    RADIO_ST,       // send data for box via radio (SPI)
+    DEBUG_ST        // send data for debug
 } states_t;
