cb54e53a6677a28d616d82a0085392ada16ca9bb
Diego Hamilton <diegomhamilton@github.com>
2019-01-16 15:31:20 -0300

    Add frequency counter feature

--- rear/main.cpp
+++ rear/main.cpp
@@ -9,8 +9,10 @@
 CAN can(PB_8, PB_9, 1000000);
 Serial serial(PA_2, PA_3, 115200);
 /* I/O pins */
+InterruptIn freq_sensor(PB_5);
 PwmOut servo(PA_6);
 /* Debug pins */
+PwmOut signal(PA_7);
 DigitalOut led(PC_13);
 
 /* Interrupt services routine */
@@ -37,6 +39,9 @@ CircularBuffer <state_t, BUFFER_SIZE> state_buffer;
 bool switch_clicked = false;
 uint8_t switch_state = 0x00;
 state_t current_state = IDLE_ST;
+uint8_t pulse_counter = 0;
+uint64_t current_period = 0, last_count = 0;
+float rpm = 0;
 
 int main()
 {
@@ -47,10 +52,13 @@ int main()
     eventThread.start(callback(&queue, &EventQueue::dispatch_forever));
     servo.period_ms(20);                        // set signal frequency to 50Hz
     servo.write(0);                             // disables servo
+    signal.period_ms(32);                        // set signal frequency to 1/0.032Hz
+    signal.write(0.5f);                          // dutycycle 50%
     can.attach(&canISR, CAN::RxIrq);
     ticker5Hz.attach(&ticker5HzISR, 0.2);
     ticker10Hz.attach(&ticker10HzISR, 0.1);
-
+    freq_sensor.fall(&frequencyCounterISR);
+    
     while (true) {
         if (state_buffer.full())
             buffer_full = true;
@@ -71,6 +79,19 @@ int main()
             case SLOWACQ_ST:
                 break;
             case RPM_ST:
+                freq_sensor.fall(NULL);         // disable interrupt
+                if (current_period != 0)
+                {
+                    rpm = 1000000*((float)pulse_counter/current_period);    //calculates frequency in Hz
+                }
+                else
+                {
+                    rpm = 0;
+                }
+                pulse_counter = 0;                          
+                current_period = 0;         //|-> reset pulses related variables
+                last_count = t.read_us();        
+                freq_sensor.fall(&frequencyCounterISR);         // enable interrupt
                 break;
             case THROTTLE_ST:
                 if (switch_clicked)
@@ -98,6 +119,7 @@ int main()
                 break;
             case DEBUG_ST:
                 serial.printf("bf=%d, cr=%d\r\n", buffer_full, switch_state);
+                serial.printf("rpm=%f\r\n", rpm);
                 break;
             default:
                 break;
@@ -120,6 +142,15 @@ void ticker5HzISR()
 void ticker10HzISR()
 {
     state_buffer.push(RPM_ST);
+}
+
+
+void frequencyCounterISR()
+{
+    pulse_counter++;
+    current_period += t.read_us() - last_count;
+    last_count = t.read_us();
+        
 }
 
 /* Interrupt handlers */
