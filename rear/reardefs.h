c8c812a16436495133e1ed06c331dd2030eca780
Diego Hamilton <diegomhamilton@github.com>
2019-01-21 18:15:07 -0300

    Add radio definitions

--- rear/reardefs.h
+++ rear/reardefs.h
@@ -1,18 +1,24 @@
 #ifndef REARDEFS_H
 #define REARDEFS_H
 
-#define SERVO_RUN         866
-#define SERVO_MID         1320
-#define SERVO_CHOKE       1780
-#define VCC               3.3
-#define R_TERM            1000
+#define SERVO_RUN           866
+#define SERVO_MID           1320
+#define SERVO_CHOKE         1780
+#define VCC                 3.3
+#define R_TERM              1000
+
+#define N_IMU               0             // imu packet counter 
+#define N_RPM               1             // rpm packet counter
+#define N_FLAG              2            // flag packet counter
+#define N_SPEED             3           // speed packet counter
+
+/* Radio definitions */
+#define NETWORK_ID          101
+#define BOXRADIO_ID         69
+#define NODE_ID             55
+#define FREQUENCY_915MHZ    91
 
 uint8_t packet_counter[4];
-
-#define N_IMU 0             // imu packet counter 
-#define N_RPM 1             // rpm packet counter
-#define N_FLAG 2            // flag packet counter
-#define N_SPEED 3           // spd packet counter
 
 typedef enum
 {
