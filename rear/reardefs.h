e3b0d782b001eba27bc0d4b9b57ebf645fce5411
Diego Hamilton <diegomhamilton@github.com>
2019-01-21 09:13:38 -0300

    Add data packet formation definitions

--- rear/reardefs.h
+++ rear/reardefs.h
@@ -12,7 +12,7 @@ uint8_t packet_counter[4];
 #define N_IMU 0             // imu packet counter 
 #define N_RPM 1             // rpm packet counter
 #define N_FLAG 2            // flag packet counter
-#define N_SPEED 3             // spd packet counter
+#define N_SPEED 3           // spd packet counter
 
 typedef enum
 {
