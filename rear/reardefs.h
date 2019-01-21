33a9c2fb4293a3ca1b4ae1df773268b3c3689752
Diego Hamilton <diegomhamilton@github.com>
2019-01-21 09:13:01 -0300

    Add data packet formation definitions

--- rear/reardefs.h
+++ rear/reardefs.h
@@ -1,10 +1,18 @@
+#ifndef REARDEFS_H
 #define REARDEFS_H
 
 #define SERVO_RUN         866
 #define SERVO_MID         1320
 #define SERVO_CHOKE       1780
 #define VCC               3.3
-#define R_THERM           1000
+#define R_TERM            1000
+
+uint8_t packet_counter[4];
+
+#define N_IMU 0             // imu packet counter 
+#define N_RPM 1             // rpm packet counter
+#define N_FLAG 2            // flag packet counter
+#define N_SPEED 3             // spd packet counter
 
 typedef enum
 {
@@ -15,3 +23,5 @@ typedef enum
     RADIO_ST,       // send data for box via radio (SPI)
     DEBUG_ST        // send data for debug
 } state_t;
+
+#endif
