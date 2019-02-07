6de0c690fd3c4344f8e8a82b12eb63eda2ac3383
Diego Hamilton <diegomhamilton@github.com>
2019-02-07 16:17:55 -0300

    Add servo modes definitions

--- rear/reardefs.h
+++ rear/reardefs.h
@@ -2,8 +2,11 @@
 #define REARDEFS_H
 
 #define SERVO_RUN           866
+#define RUN_MODE            0x01
 #define SERVO_MID           1320
+#define MID_MODE            0x00
 #define SERVO_CHOKE         1780
+#define CHOKE_MODE          0x02
 #define VCC                 3.3
 #define R_TERM              1000
 
@@ -17,7 +20,6 @@
 #define BOXRADIO_ID         69
 #define NODE_ID             55
 #define FREQUENCY_915MHZ    91
-#define FAILSAFE_PERIOD     1
 
 uint8_t packet_counter[4];
 
